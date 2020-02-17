ad450ed08b27b4d19a6dee8a775627c356444f4a
Sebastian Delima <sebastiandelima@Cactus1.local>
2020-02-17 16:02:34 -0500

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,13 @@
 # Enter your object-oriented solution here!
+# a number that is divided by its multiple and that the remainder is 0
+
+def findMultiples
+    arr = []
+    multiples = [3, 5]
+    101.times { |num| 
+    arr.push(num)
+    counter += 1
+    }
+    arr.select{|num| 
+     return num / 3 || 5 == 0}
+end
